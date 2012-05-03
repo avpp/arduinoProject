d7e6a4552e30b3b8fe6c2a100049a817c4e26253
Alexey <nuzhaleks2@rambler.ru>
2012-05-03 16:49:34 +0400

    some changes

--- controlClient.py
+++ controlClient.py
@@ -2,8 +2,17 @@ import socket
 import sys
 import cv2
 import threading
+
+#create cv2 window for input
+windowName = "ControlWindow"
+print "try to create window"
+cv2.namedWindow(windowName)
+print "try to listen"
+cv2.waitKey(1)
+print "create window"
+
 import videoStream
-#import gtk
+
 ##
 ## control by opencv window
 ## press ESC for disconnect from server and close window
@@ -15,27 +24,33 @@ import videoStream
 ## press SPACE to send stop command (important in fixrd type of control)
 ##
 class listener (threading.Thread):
-	def __init__(self, sock, da):
+	def __init__(self, sock):
 		self.sock = sock
 		self.alive = True
-		self.da = da
+		self.stream = None
 		threading.Thread.__init__(self)
+	def stop(self):
+		alive = False
 	def run(self):
 		while (self.alive):
 			buf = None
 			try:
 				buf = self.sock.recv(1024)
 			except KeyboardInterrupt:
-				self.alive = false
+				self.alive = False
 				break
-			print "accept ", buf
+			except socket.error:
+				self.alive = False
+				break
+			print "accept '", buf, "'"
 			buf = buf[buf.find("caps"):len(buf)]
 			if (buf.startswith("caps")):
 				CAPSparam = buf[4:len(buf)]
 				print "we have caps = ", CAPSparam
-				stream = videoStream.Client(da, CAPSparam)
-				stream.start()
+				self.stream = videoStream.Client(CAPSparam)
+				self.stream.start()
 				print "vido create"
+		self.stream.stop()
 host = 'localhost'
 port = 4444
 if __name__ == "__main__":
@@ -58,33 +73,8 @@ changeButton = 1048586
 escapeButton = 1048603
 lastState='x'
 count = 0
-windowName = "ControlWindow"
-print "try to create window"
-cv2.namedWindow(windowName)
-print "try to listen"
-cv2.waitKey(1)
-print "create window"
 
-import gtk
-
-#gg = gtk.window_list_toplevels()
-wnd = gtk.Window()
-#for i in range(len(gg)):
-#	print gg[i].get_title()
-#	if (windowName == gg[i].get_title()):
-#		wnd = gg[i]
-#		break
-if (wnd == None):
-	print "error, no window", windowName
-	exit()
-vb = gtk.VBox()
-wnd.add(vb)
-da = gtk.DrawingArea()
-da.set_size_request(640,480)
-vb.add(da)
-wnd.connect("destroy", gtk.main_quit, "WM destroy")
-wnd.show_all()
-l = listener(s, da)
+l = listener(s)
 l.start()
 
 
@@ -108,9 +98,5 @@ while True:
 		print "now_send", newState
 		s.send(newState)
 		lastState = newState
-#	c = raw_input()
-#	print "now send ", c
-#	s.send(c)
-#	if (c == 'exit'):
-#		break
 s.close()
+l.stop()
